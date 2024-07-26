15f82b5028fb7fff5c92d910c90e6786595c73b7
Sugakesshaa <sugakesshaa@gmail.com>
2024-07-26 17:33:04 +0530

    stone: switch with oos libperfmgr

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -237,7 +237,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+
 # Sepolicy
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
